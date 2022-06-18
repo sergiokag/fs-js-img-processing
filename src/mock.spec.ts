2f8f32581f57f516ad24634fc7703ccb92f236f6
Sergio Kagiema <sergio_kagiema@live.com>
2022-06-19 01:20:33 +0300

    Initial commit

---
+++ src/mock.spec.ts
@@ -0,0 +1,5 @@
+describe('testing jasmine configuration', () => {
+    it('should work', () => {
+        expect(1 + 5).toBe(6);
+    });
+});
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', function (_, res) {
+    res.send('Hello World!!');
+});
+
+app.listen(port, () => console.log(`Listening at port ${port}`));
